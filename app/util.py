6a23b6a67e5c0025aaf3f253d927245ef3a58976
farseen <farseenmanekhan1232@gmail.com>
2024-03-28 11:52:09 +0530

    updated project '

--- app/util.py
+++ app/util.py
@@ -10,6 +10,7 @@ def mz_to_molecular_mass(m_z, z):
 
 
 def process_mzml_file(file_path):
+    # Load and process .mzML file to extract m/z and charge values
     exp = MSExperiment()
     MzMLFile().load(file_path, exp)
     mz_values, charges = [], []
@@ -22,13 +23,15 @@ def process_mzml_file(file_path):
 
 def predict_substance(mz, charge):
     molecular_mass = mz_to_molecular_mass(mz, charge)
+    # Load model
     model = load_model()
+    # Assuming the model expects a DataFrame
     df = pd.DataFrame({"molecular_mass": [molecular_mass]})
     prediction = model.predict(df)
     return prediction[0]
 
 
 def load_model():
-    model_path = "../models/model.pkl"
+    model_path = "./models/model.pkl"
     model = joblib.load(model_path)
     return model
